d9f2e25ff67dd595aa987c2e7ad400bd3cead8da
Fabricio Dorneles <fabriciodorneles@gmail.com>
2020-09-22 22:17:53 -0300

    little changes

--- src/components/catalog.tsx
+++ src/components/catalog.tsx
@@ -1,16 +1,21 @@
 import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import api from '../services/api';
-import { IProduct } from '../store/modules/cart/types';
+import { IState } from '../store';
+import { ICartItem, IProduct } from '../store/modules/cart/types';
 import CatalogItem from './catalogItem';
 
 const Catalog: React.FC = () => {
   const [catalog, setCatalog] = useState<IProduct[]>([]);
+  const cart = useSelector<IState, ICartItem[]>(state => state.cart.items)
 
   useEffect(() => {
     api.get('products').then(response => {
       setCatalog(response.data)
     })
   }, []);
+
+  
 
   return (
     <main>
